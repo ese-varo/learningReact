2ed9ab6848fe9e49e98774ad4b222ef36f9a51b2
Alvaro Padilla <varoboss.ap@gmail.com>
2020-08-26 18:56:51 -0500

    Finish color organizer

--- color-organizer/src/components/Color.js
+++ color-organizer/src/components/Color.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import { FaTrash } from 'react-icons/fa';
 import StarRating from './StarRating';
+import { useColors } from '../color-hooks';
 
-export default function Color({
-  id,
-  title,
-  color,
-  rating,
-  onRemove = f => f,
-  onRate = f => f
-}) {
+export default function Color({ id, title, color, rating }) {
+  const { rateColor, removeColor } = useColors();
   return (
     <section>
       <h1>{title}</h1>
-      <button onClick={() => onRemove(id)}>
-        <FaTrash />
-      </button>
+      <button onClick={() => removeColor(id)}>X</button>
       <div style={{ height: 50, backgroundColor: color }} />
       <StarRating
         selectedStars={rating}
-        onRate={rating => onRate(id, rating)}
+        onRate={rating => rateColor(id, rating)}
       />
     </section>
   );
--- color-organizer/src/index.js
+++ color-organizer/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { ColorProvider } from './color-hooks.js';
+import { render } from 'react-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <ColorProvider>
     <App />
-  </React.StrictMode>,
+  </ColorProvider>,
   document.getElementById('root')
 );
 
