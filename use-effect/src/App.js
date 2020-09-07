31c281107db66d8b1cf6a8097acd3fe51fcd97c1
Alvaro Padilla <varoboss.ap@gmail.com>
2020-09-06 20:08:57 -0500

    Add use-effect hook exercise

---
+++ use-effect/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Checked from "./components/Checkbox";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Checked />
+      </header>
+    </div>
+  );
+}
+
+export default App;
