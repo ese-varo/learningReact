2854f6316730f134b5e9d89f72a6047011fa2f6c
Alvaro Padilla <varoboss.ap@gmail.com>
2020-09-08 20:01:08 -0500

    Add deep checking dependencies part-ii
    This part is improved using the hooks for memoizing values and objects
    (functions, arrays, objects), using useMemo and useCallback
    respectively.

--- deep-checking-dependencies/src/components/WordCount.js
+++ deep-checking-dependencies/src/components/WordCount.js
@@ -1,10 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useCallback } from "react";
 import { useAnyKeyToRender } from "./../hooks";
 
 export default function WordCount({ children = "", hola = "", casa }) {
   useAnyKeyToRender();
 
-  const words = children.split(" ");
+  const words = useMemo(() => children.split(" "), [children]);
+
+  const fn = useCallback(() => {
+    console.log("hello");
+    console.log("world");
+  }, []);
+
+  useEffect(() => {
+    console.log(`hey these are the words array: ${words}`);
+    console.log("fresh render function test");
+    fn();
+  }, [fn]);
 
   useEffect(() => {
     console.log("fresh render");
